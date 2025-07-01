6d5e71f1fb82f1d6d8464c5b7d171ad9d3eae6ec
RuperLip <evgeniiruperlip@gmail.com>
2025-07-01 22:38:06 +0300

    Add files via upload

---
+++ Т01D01/quest3.sh
@@ -0,0 +1,8 @@
+mv door_management_fi door_management_files //изменил расширение папки
+cd door_management_files //вошел в папку
+mkdir configuration //создал папку
+mkdir door_map //создал папку
+mkdir door_logs //создал папку
+mv *.conf door_configuration //переместить все файлы с расширением conf в папку
+mv *.log door_logs //переместить все файлы с расширением log в папку
+mv door_map_1.1 door_map //переместить файл в папку
